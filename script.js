13a098bdf7739d77f887e6c92df4b0e6ba62890c
Matheus Augusto <mathieus_n.7@hotmail.com>
2023-03-07 12:51:32 -0300

    Use flexbox to make the divs appear as a grid [!]
    Given the way flexbox works, throw the divs into the
    container was causing problems, so we had to elabo-
    rate a better solution.
    
    The solution was to implement two functions: one to
    generate rows; another to generate squares
    inside the said rows.

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-function createGrid(){
-    const container = document.querySelector('div');
+const cont = [];
 
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.setAttribute('style', 
-                        `backgroundColor: white; 
-                         border: 1px solid black;
-                         borderCollapse: collapse;
-                         height: 50px;
-                         width: 50px;
-                         margin: 0;
-                         padding: 0;`);
-
-    return container.appendChild(square);
+for (let i=0; i<16; i++){
+    cont.push(`row${i}`);
 }
 
-for (let i=0; i < 256; i++) {
-   createGrid();
-}
+for (let j=0; j < cont.length; j++){
+    let corridor = cont[j];
+    
+    function createRow(corridor){
+        const container = document.querySelector('div');
+    
+        corridor = document.createElement('div');
+        corridor.classList.add(`${cont[j]}`);
+        corridor.setAttribute('style', `display: flex;
+                                       border: 1px solid black;
+                                       height: 50px;
+                                       width: 800px`);
+        
+        return container.appendChild(corridor);
+    }
 
+    createRow(corridor)
+
+    for (let k=0; k < cont.length; k++){
+        
+        function createSquare(corridor){
+            corridor = document.querySelector(`.${cont[j]}`);
+            
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.setAttribute('style', 
+                                `backgroundColor: white; 
+                                 border: 1px solid black;
+                                 borderCollapse: collapse;
+                                 height: 50px;
+                                 width: 50px;
+                                 margin: 0;
+                                 padding: 0;
+                                 flex: 1 1 auto`);
+        
+            return corridor.appendChild(square);
+        }
+
+        createSquare(corridor)
+        
+    }
+
+}
 
 const squares = document.querySelectorAll(".square");
 
