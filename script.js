eee12f031337b3e8658c3cfef934e0753b07b7e0
Matheus Augusto <mathieus_n.7@hotmail.com>
2023-03-09 14:53:48 -0300

    Major changes to how the code is organized
    Almost everything is now a function or part of a function.
    
    The functions to generate rows and squares, for example,
    are part of the function 'createGrid'.
    
    Also, changing color is now a function called 'paint'.
    
    The button that asks how many squares per side and stores
    value, despite not being a function, it calls more than
    one function.
    
    This approach allowed the code to be cleaner and easier
    to understand.

--- script.js
+++ script.js
@@ -1,78 +1,104 @@
-let valor;
+let valor = 16,
+    cont = [];
+
+function numberOfSquares(){
+    cont = [];
+
+    for (let i=0; i < valor; i++){
+        cont.push(`row${i}`);
+    }
+
+    return cont
+}
+
+function createGrid(){
+    numberOfSquares();
+    
+    for (let j=0; j < cont.length; j++){
+        let corridor = cont[j];
+        
+        function createRow(corridor){
+            const container = document.querySelector('div');
+        
+            corridor = document.createElement('div');
+            corridor.classList.add(`${cont[j]}`);
+            corridor.setAttribute('style', `display: flex;
+                                        border: 1px solid black;
+                                        height: 50px;
+                                        width: 800px`);
+            
+            return container.appendChild(corridor);
+        }
+
+        createRow(corridor)
+
+        for (let k=0; k < cont.length; k++){
+            
+            function createSquare(corridor){
+                corridor = document.querySelector(`.${cont[j]}`);
+                
+                const square = document.createElement('div');
+                square.classList.add('square');
+                square.setAttribute('style', 
+                                    `backgroundColor: white; 
+                                    border: 1px solid black;
+                                    borderCollapse: collapse;
+                                    height: 50px;
+                                    width: 50px;
+                                    margin: 0;
+                                    padding: 0;
+                                    flex: 1 1 auto`);
+            
+                return corridor.appendChild(square);
+            }
+
+            createSquare(corridor)
+            
+        }
+
+    }
+}
+
+function paint(){
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach(square => {
+
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = "orange";     
+        }, false);
+    })
+}
+
+function empty(element) {
+    element.replaceChildren(); 
+}
+
+createGrid();
+
+paint();
 
 const btn = document.querySelector('.btn');
 
-    btn.addEventListener('click', (e) => {
-        valor = prompt(`How many squares per side?
-                            
-    (lowest value is 2, highest value is 100!)
+btn.addEventListener('click', (e) => {
+    const parent = document.querySelector(".container");
+    empty(parent);
+    
+    valor = prompt(`How many squares per side?
+                        
+(lowest value is 2, highest value is 100!)
 
-    `);
+`);
 
-        while ((valor < 2) || (valor > 100)){
-            valor = prompt(`Seems like you picked the wrong number
-            
-    Try again!`);
-    }
-    });
-
-console.log(valor);
-
-const cont = [];
-
-for (let i=0; i<16; i++){
-    cont.push(`row${i}`);
+    while ((valor < 2) || (valor > 100)){
+        valor = prompt(`Seems like you picked the wrong number
+        
+Try again!`);
 }
 
-for (let j=0; j < cont.length; j++){
-    let corridor = cont[j];
+    valor = Number(valor);
     
-    function createRow(corridor){
-        const container = document.querySelector('div');
+    createGrid();
     
-        corridor = document.createElement('div');
-        corridor.classList.add(`${cont[j]}`);
-        corridor.setAttribute('style', `display: flex;
-                                       border: 1px solid black;
-                                       height: 50px;
-                                       width: 800px`);
-        
-        return container.appendChild(corridor);
-    }
-
-    createRow(corridor)
-
-    for (let k=0; k < cont.length; k++){
-        
-        function createSquare(corridor){
-            corridor = document.querySelector(`.${cont[j]}`);
-            
-            const square = document.createElement('div');
-            square.classList.add('square');
-            square.setAttribute('style', 
-                                `backgroundColor: white; 
-                                 border: 1px solid black;
-                                 borderCollapse: collapse;
-                                 height: 50px;
-                                 width: 50px;
-                                 margin: 0;
-                                 padding: 0;
-                                 flex: 1 1 auto`);
-        
-            return corridor.appendChild(square);
-        }
-
-        createSquare(corridor)
-        
-    }
-
-}
-
-const squares = document.querySelectorAll(".square");
-
-squares.forEach(square => {
-
-    square.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = "orange";     
-    }, false);
-})
+    paint();
+});
